81274092b22609922c310a2f553b4c9a0ebb4431
cfalcant <cfalcant@gmail.com>
2018-01-05 15:57:57 -0700

    completed through Pt1 Challenge3

--- part1/server.js
+++ part1/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require('path');
+// const fs = require('fs');
 
 // ### Challenge 1:
 // Create a GET route for "/hello" that returns the response "Hello!".
-app.get('/hello', function(req, res) {
-  res.send("Hello!");
-});
+
+// app.get('/hello', function(req, res) {
+//   res.send("Hello!");
+// });
 
 // ### Challenge 2:
 // Create a POST route for "/create/:name" that returns a json object that looks like this:
@@ -15,12 +18,18 @@ app.get('/hello', function(req, res) {
 //   "name":"the name they pass in the route"
 // }
 
-app.get('/create/:name', function(req, res) {
-  let object = {
-    id: 1,
-    name:"the name they pass in the route"};
-  res.json(object);
-});
+// app.get('/create/:name', function(req, res) {
+//   let object = {
+//     id: 1,
+//     name:"the name they pass in the route"};
+//   res.json(object);
+// });
+
+// ### Challenge 3:
+// Create a GET route for "/" that returns the index.html file.
+   app.get('/', function (req, res) {
+     res.sendFile(path.join(__dirname, './', 'index.html'));
+   });
 
 
 app.use(function(req, res) {
