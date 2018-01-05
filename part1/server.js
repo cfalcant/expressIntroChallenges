1ed8b6ce85552f9ea22e323eceafd077221dc0ed
cfalcant <cfalcant@gmail.com>
2018-01-05 14:30:03 -0700

    completed through Pt 1 Chall2

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,26 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+// ### Challenge 1:
+// Create a GET route for "/hello" that returns the response "Hello!".
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+// ### Challenge 2:
+// Create a POST route for "/create/:name" that returns a json object that looks like this:
+// {
+//   "id":1,
+//   "name":"the name they pass in the route"
+// }
+
+app.get('/create/:name', function(req, res) {
+  let object = {
+    id: 1,
+    name:"the name they pass in the route"};
+  res.json(object);
+});
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
