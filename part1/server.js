6add26203323ee07d38814042fb2a128778e36e7
cfalcant <cfalcant@gmail.com>
2018-01-07 11:46:24 -0700

    completed through Pt1 Challenge4

--- part1/server.js
+++ part1/server.js
@@ -7,9 +7,9 @@ var path = require('path');
 // ### Challenge 1:
 // Create a GET route for "/hello" that returns the response "Hello!".
 
-// app.get('/hello', function(req, res) {
-//   res.send("Hello!");
-// });
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+});
 
 // ### Challenge 2:
 // Create a POST route for "/create/:name" that returns a json object that looks like this:
@@ -18,18 +18,33 @@ var path = require('path');
 //   "name":"the name they pass in the route"
 // }
 
-// app.get('/create/:name', function(req, res) {
-//   let object = {
-//     id: 1,
-//     name:"the name they pass in the route"};
-//   res.json(object);
-// });
+app.get('/create/:name', function(req, res) {
+  let object = {
+    id: 1,
+    name:"the name they pass in the route"};
+  res.json(object);
+});
 
 // ### Challenge 3:
-// Create a GET route for "/" that returns the index.html file.
+Create a GET route for "/" that returns the index.html file.
    app.get('/', function (req, res) {
      res.sendFile(path.join(__dirname, './', 'index.html'));
    });
+
+// ### Challenge 4:
+// Create a GET route for "/verify/:age" that checks if the age is greater than 13. If it is
+// then it sends a 200 status response (for all good). If it is not greater than 13 then send a
+// 403 status code. For more information on what status codes mean refer to this -
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+app.get('/verify/:age', function (req, res) {
+  if (req.params.age > 13) {
+    // res.send('Status code 200')
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403)
+  }
+});
+
 
 
 app.use(function(req, res) {
